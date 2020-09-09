f178ba8ceba6077ad1ce01b63ce972f0f76df837
Caio Siqueira <csiqueirasilva@gmail.com>
2020-09-09 18:59:37 -0300

    Removed ffmpeg dependency; Compiled a C++ application that exports png screenshots

---
+++ bin/main.cpp
@@ -0,0 +1,192 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/* 
+    src: https://stackoverflow.com/a/30138664/1988747 
+    edited to flush data to stdout and parameters to crop desktop image (from main display)
+*/
+
+#include <io.h>
+#include <fcntl.h>
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+#include <Wincodec.h>             // we use WIC for saving images
+#include <d3d9.h>                 // DirectX 9 header
+#pragma comment(lib, "d3d9.lib")  // link to DirectX 9 library
+
+#define WIDEN2(x) L ## x
+#define WIDEN(x) WIDEN2(x)
+#define __WFILE__ WIDEN(__FILE__)
+#define HRCHECK(__expr) {hr=(__expr);if(FAILED(hr)){wprintf(L"FAILURE 0x%08X (%i)\n\tline: %u file: '%s'\n\texpr: '" WIDEN(#__expr) L"'\n",hr, hr, __LINE__,__WFILE__);goto cleanup;}}
+#define RELEASE(__p) {if(__p!=nullptr){__p->Release();__p=nullptr;}}
+
+#define _DEBUG 0
+
+UINT bytesPerPixel = 4;
+UINT Height = 0;
+UINT Width = 0;
+UINT exportHeight = 0;
+UINT exportWidth = 0;
+IDirect3D9* d3d = nullptr;
+IDirect3DDevice9* device = nullptr;
+IDirect3DSurface9* surface = nullptr;
+D3DPRESENT_PARAMETERS parameters = { 0 };
+D3DDISPLAYMODE mode;
+D3DLOCKED_RECT rc;
+RECT cropRect;
+UINT pitch;
+LPBYTE output = nullptr;
+UINT adapter = D3DADAPTER_DEFAULT;
+LONG left;
+LONG top; 
+LONG right;
+LONG bottom;
+LPBYTE shot;
+IWICImagingFactory* factory = nullptr;
+IWICBitmapEncoder* encoder = nullptr;
+
+void sendIStreamToOutput (IStream * pIStream) {
+
+    STATSTG sts;
+    pIStream->Stat(&sts, STATFLAG_DEFAULT);
+    ULARGE_INTEGER uli = sts.cbSize;
+    LARGE_INTEGER zero;
+    zero.QuadPart = 0;
+    ULONG size = (ULONG)uli.QuadPart;
+    char* bits = new char[size];
+    ULONG written;
+    pIStream->Seek(zero, STREAM_SEEK_SET, NULL);
+    pIStream->Read(bits, size, &written);
+    
+    std::ostream& lhs = std::cout;
+
+    _setmode(_fileno(stdout), _O_BINARY);
+
+#if _DEBUG == 1
+    FILE* f;
+    fopen_s(&f, "out2.png", "wb");
+    fwrite(bits, size, 1, f);
+    fclose(f);
+#else
+    lhs.write(bits, size);
+    fflush(stdout);
+#endif
+
+    delete[] bits;
+ }
+
+HRESULT SavePixelsToFile32bppPBGRA()
+{
+    HRESULT hr = S_OK;
+
+    IWICBitmapFrameEncode* frame = nullptr;
+    IWICStream* streamOut = nullptr;
+    IStream * streamIn = nullptr;
+    GUID pf = GUID_WICPixelFormat32bppPBGRA;
+    BOOL coInit = CoInitialize(nullptr);
+    
+    HRCHECK(CoCreateInstance(CLSID_WICImagingFactory, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory)));
+    HRCHECK(factory->CreateEncoder(GUID_ContainerFormatPng, nullptr, &encoder));
+    HRCHECK(factory->CreateStream(&streamOut));
+    HRCHECK(CreateStreamOnHGlobal(NULL, true, &streamIn));
+    HRCHECK(streamOut->InitializeFromIStream(streamIn));
+    HRCHECK(encoder->Initialize(streamOut, WICBitmapEncoderNoCache));
+    HRCHECK(encoder->CreateNewFrame(&frame, nullptr)); // we don't use options here
+    HRCHECK(frame->Initialize(nullptr)); // we dont' use any options here
+    HRCHECK(frame->SetSize(exportWidth, exportHeight));
+    HRCHECK(frame->SetPixelFormat(&pf));
+    HRCHECK(frame->WritePixels(exportHeight, pitch, pitch * exportHeight, shot));
+    HRCHECK(frame->Commit());
+    HRCHECK(encoder->Commit());
+
+    sendIStreamToOutput(streamOut);
+
+cleanup:
+    RELEASE(streamIn);
+    RELEASE(streamOut);
+    RELEASE(frame);
+    RELEASE(encoder);
+    RELEASE(factory);
+    if (coInit) CoUninitialize();
+    
+    return hr;
+}
+
+HRESULT Direct3D9TakeScreenshots()
+{
+    HRESULT hr = S_OK;
+
+    // get the data
+    HRCHECK(device->GetFrontBufferData(0, surface));
+    
+    // copy it into our buffers
+    HRCHECK(surface->LockRect(&rc, &cropRect, D3DLOCK_NO_DIRTY_UPDATE | D3DLOCK_READONLY));
+    CopyMemory(shot, rc.pBits, pitch * bottom - (top * pitch + bytesPerPixel * left));
+    HRCHECK(surface->UnlockRect());
+    
+    HRCHECK(SavePixelsToFile32bppPBGRA());
+
+cleanup:
+    
+    return hr;
+}
+
+int main(int argc, char ** argv)
+{
+    right = atoi(argv[3]);
+    bottom = atoi(argv[4]);
+    left = atoi(argv[1]);
+    top = atoi(argv[2]);
+
+    // init D3D and get screen size
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);
+    d3d->GetAdapterDisplayMode(adapter, &mode);
+
+    Height = mode.Height;
+    Width = mode.Width;
+
+    parameters.Windowed = TRUE;
+    parameters.BackBufferCount = 1;
+    parameters.BackBufferHeight = Height;
+    parameters.BackBufferWidth = Width;
+    parameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    parameters.hDeviceWindow = NULL;
+
+    // create device & capture surface
+    d3d->CreateDevice(adapter, D3DDEVTYPE_HAL, NULL, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &parameters, &device);
+    device->CreateOffscreenPlainSurface(Width, Height, D3DFMT_A8R8G8B8, D3DPOOL_SYSTEMMEM, &surface, nullptr);
+
+    cropRect.left = left;
+    cropRect.top = top;
+    cropRect.right = right;
+    cropRect.bottom = bottom;
+
+    exportHeight = abs(cropRect.top - cropRect.bottom);
+    exportWidth = abs(cropRect.left - cropRect.right);
+
+    pitch = Width * bytesPerPixel;
+
+    shot = new BYTE[pitch * exportHeight];
+
+    
+
+    
+    
+#if _DEBUG == 0
+    while(true) {
+#endif
+        Direct3D9TakeScreenshots();
+#if _DEBUG == 0
+    }
+#endif
+
+    delete[] shot;
+    RELEASE(surface);
+    RELEASE(device);
+    RELEASE(d3d);
+
+    return 0;
+}
--- src/ScreenCapture.js
+++ src/ScreenCapture.js
@@ -1,82 +1,40 @@
-const ffmpeg = require("ffmpeg-static");
-const { spawn } = require("child_process");
-const tempFile = require('./TempFile');
+const { spawn, execFile } = require("child_process");
 const fs = require('fs');
 const Files = require('./Files');
 const Logger = require('./Logger');
 const Config = require('./Config');
 
-const SEC_RENEW = 240 * 1000;
-const DELAY_ERASE_OLD = 10 * 1000;
+// Start of Image
+const soi = Buffer.from([0x89, 0x50, 0x4e, 0x47]);
+// End of Image
+const eoi = Buffer.from([0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82]);
 
-const streamTable = {};
-let streamId = 0;
-
-function createStream(filename, win, group) {
-
+async function captureWindow (win, group) {
+	
 	const elementId = group.name;
-
+	
 	const config = Config.get();
 
 	const offsetX = parseInt(config.screen.width * group.screen.offsetX);
 	const offsetY = parseInt(config.screen.height * group.screen.offsetY);
 	const sizeX = parseInt(config.screen.width * group.screen.sourceWidth);
 	const sizeY = parseInt(config.screen.height * group.screen.sourceHeight);
-	const quality = config.videoQuality;
-	const fps = config.fps;
 	
-	const proc = spawn(
-		ffmpeg,
-	  ["-f", "gdigrab", "-framerate", fps, "-video_size", sizeX + "x" + sizeY, "-offset_x", offsetX, "-offset_y", offsetY, "-draw_mouse", "0", "-i", "title=Path of Exile", "-preset", "ultrafast", "-tune", "zerolatency", "-f", "mjpeg", "-huffman", "optimal", "-qscale", quality, "-"],
-	  { stdio: "pipe" }
-	);
-
+	let args = [offsetX, offsetY, offsetX + sizeX, offsetY + sizeY];
+	
+	if(config.debug) {
+		console.log(args);
+	}
+	
+	const proc = spawn('bin/capture.exe', args);
 	const stream = proc.stdout;
 	
-	stream.streamId = streamId++;
-	
-	const file = fs.createWriteStream(filename);
-
-	stream.pipe(file);
-
-	setTimeout(() => {
-
-		Logger.log('restarting capture process');
-
-		captureWindow(win, group);
-		
-		setTimeout(() => {
-
-			proc.kill();
-
-			Files.erase(filename);
-
-		}, DELAY_ERASE_OLD);
-		
-	}, SEC_RENEW);
-
-	return stream;
-}
-
-// Start of Image
-const soi = Buffer.from([0xff, 0xd8]);
-
-// End of Image
-const eoi = Buffer.from([0xff, 0xd9]);
-
-async function captureWindow (win, group) {
-	
-	const elementId = group.name;
-	const file = await tempFile.load();
-	let fpath = file.data.path;
-	let previousStream = streamTable[elementId];
-	let stream = createStream(fpath, win, group);
 	let data = [];
 	
 	stream.on("data", chunk => {
 		
-		var start = chunk.indexOf(soi);
-		var end = chunk.indexOf(eoi);
+		//var start = chunk.indexOf(soi);
+		var end = chunk.indexOf(eoi, chunk.length - eoi.length);
 
 		data.push(chunk);
 		
